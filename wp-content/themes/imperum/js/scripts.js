a7faee76aa3429e07074dd19bd8928f1d1d1795f
Milos Jovancevic <jovancevicmilos@gmail.com>
2017-11-08 12:16:49 +0100

    added logic for changing color of hex values

--- wp-content/themes/imperum/js/scripts.js
+++ wp-content/themes/imperum/js/scripts.js
@@ -1,5 +1,49 @@
 (function ($, root, undefined) {
 
+	function hexToRgb (hex) {
+
+		if (hex.charAt && hex.charAt(0) === '#') {
+			hex = removeHash(hex)
+		}
+
+	if (hex.length === 3) {
+		hex = expand(hex)
+	}
+
+	var bigint = parseInt(hex, 16)
+	var r = (bigint >> 16) & 255
+	var g = (bigint >> 8) & 255
+	var b = bigint & 255
+
+	return [r, g, b]
+	}
+
+	function removeHash (hex) {
+
+		var arr = hex.split('')
+		arr.shift()
+		return arr.join('')
+	}
+
+	function expand (hex) {
+
+		return hex
+	    .split('')
+	    .reduce(function (accum, value) {
+
+		return accum.concat([value, value])
+	    }, [])
+	    .join('')
+	}
+
+	function contrast(hex) {
+		var rgb = hexToRgb(hex);
+		var o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) /1000);
+
+		return (o <= 180) ? 'dark' : 'light';
+	}
+
+
 	$(function () {
 
 		//SMALL NAVIGATION TOGGLE
@@ -313,22 +357,28 @@
 	  	$('#projectDuration').text(projectDuration);
 	  	$('#shortDescription').text(shortDescription);
 
-	  	var link = $('#projectLink').text();
+	  	var linkInserted = $('#projectLink').text();
 
 	  	var projectLinkUrl = $('#projectLink').text().split('//')[1].replace(/\//g, '');
 	  	$('#projectLink').text('');
-	  	var aTag = $('#projectLink').append('<a href="' + link + '">' + projectLinkUrl + '</a>');
+	  	var aTag = $('#projectLink').append('<a rel="noopener noreferrer" target="_blank" href="' + linkInserted + '">' + projectLinkUrl + '</a>');
 
 
 	  	var colorOne = currentElement.data('colorone');
 		var colorTwo = currentElement.data('colortwo');
 		var colorThree = currentElement.data('colorthree');
 		var colorFour = currentElement.data('colorfour');
+		var activeShadow = "#c09858 1px 0px, #c09858 0px 1px, #c09858 0px -1px, #c09858 -1px 0px";
 
 		$("#colorOne span").css("color", "#" + colorOne);
 		$("#colorTwo span").css("color", "#" + colorTwo);
 		$("#colorThree span").css("color", "#" + colorThree);
 		$("#colorFour span").css("color", "#" + colorFour);
+
+		$("#colorOne .project-icon").css('textShadow', contrast(colorOne) === 'light' ? activeShadow : 'none');
+		$("#colorTwo .project-icon").css('textShadow', contrast(colorTwo) === 'light' ? activeShadow : 'none');
+		$("#colorThree .project-icon").css('textShadow', contrast(colorThree) === 'light' ? activeShadow : 'none');
+		$("#colorFour .project-icon").css('textShadow', contrast(colorFour) === 'light' ? activeShadow : 'none');
 
 		$("#colorOne h5").text(colorOne);
 		$("#colorTwo h5").text(colorTwo);
