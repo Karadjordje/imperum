b2d4b3b3c1108031413a2015b8b586eda6143dd5
damirkozic <damir.kozic@gmail.com>
2017-11-13 13:31:59 +0100

    Zavrsena stranica VideoBlogSingle

--- wp-content/themes/imperum/comments.php
+++ wp-content/themes/imperum/comments.php
@@ -1,23 +1,16 @@
-<div class="row">
-	<?php if (post_password_required()) : ?>
-	<p><?php _e( 'Post is password protected. Enter the password to view any comments.', 'html5blank' ); ?></p>
-</div>
 
-	<?php return; endif; ?>
+
+<h3 class="commHL">Comments</h3>
+<?php if (post_password_required()) : ?>
+	<p><?php _e( 'Post is password protected. Enter the password to view any comments.', 'html5blank' ); ?></p>
+<?php
+	return;
+	endif;
+?>
 
 <?php if (have_comments()) : ?>
 
-
-<div class="col-xs-12 col-sm-12 col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2">
-	<div class="row">
-		<div class="col-xs-10 col-sm-9 col-md-10 col-lg-10 col-xs-offset-2 col-sm-offset-3 col-md-offset-3 col-lg-offset-4">
-			<div class="comment-one">
-				<h3>Comments</h3>
-				<?php wp_list_comments('type=comment&callback=html5blankcomments'); // Custom callback in functions.php ?>
-			</div>
-		</div>
-	</div>
-</div>
+	<?php wp_list_comments('type=comment&callback=html5blankcomments'); // Custom callback in functions.php ?>
 
 <?php elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
--- wp-content/themes/imperum/functions.php
+++ wp-content/themes/imperum/functions.php
@@ -346,31 +346,54 @@ function html5blankcomments($comment, $args, $depth)
 		$add_below = 'div-comment';
 	}
 ?>
-    <!-- heads up: starting < for the html tag (li or div) in the next line: -->
-    <<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
-	<?php if ( 'div' != $args['style'] ) : ?>
-	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-	<?php endif; ?>
-	<div class="comment-author vcard">
-	<?php printf(__('<h4>%s</h4>'), get_comment_author_link()) ?>
-	</div>
-<?php if ($comment->comment_approved == '0') : ?>
-	<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
-	<br />
-<?php endif; ?>
-
-	<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-		<?php
-			 ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
-		?>
-	</div>
-
-	<?php comment_text() ?>
-
-	<?php if ( 'div' != $args['style'] ) : ?>
-	</div>
-	<?php endif; ?>
+    <div class="comment-one clearfix">
+        <?php printf(__('<h4>%s</h4>'), get_comment_author_link()) ?>
+        <?php if ( 'div' != $args['style'] ) : ?>
+            <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+                <?php comment_text() ?>
+            </div>
+    	<?php endif; ?>
+    </div>
 <?php }
+
+/*function html5blankcomments($comment, $args, $depth)
+{
+    $GLOBALS['comment'] = $comment;
+    extract($args, EXTR_SKIP);
+
+    if ( 'div' == $args['style'] ) {
+        $tag = 'div';
+        $add_below = 'comment';
+    } else {
+        $tag = 'p';
+        $add_below = 'div-comment';
+    }
+?>
+    <!-- heads up: starting < for the html tag (li or div) in the next line: -->
+    <!-- <<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>"> -->
+    <?php if ( 'div' != $args['style'] ) : ?>
+       <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+    <?php endif; ?>
+    <div class="comment-author vcard">
+       <?php printf(__('<h4>%s</h4>'), get_comment_author_link()) ?>
+    </div>
+    <?php if ($comment->comment_approved == '0') : ?>
+        <em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
+        <br />
+    <?php endif; ?>
+
+    <div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
+        <?php
+             ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
+        ?>
+    </div>
+
+    <?php comment_text() ?>
+
+    <?php if ( 'div' != $args['style'] ) : ?>
+    </div>
+    <?php endif; ?>
+<?php }*/
 
 /*------------------------------------*\
 	Actions + Filters + ShortCodes
