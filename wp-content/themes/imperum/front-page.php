e0d13664d376e69210cbd92ddd28f03f9945b0fa
damirkozic <damir.kozic@gmail.com>
2017-11-23 13:22:56 +0100

    checkbox-ovi na leanding strani

--- wp-content/themes/imperum/front-page.php
+++ wp-content/themes/imperum/front-page.php
@@ -214,7 +214,7 @@
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2 checklist">
                         <div class="row">
-                            <div class="col-xs-10  col-sm-9 col-xs-offset-2 col-sm-offset-3 checkbox-content" id="category-checkboxes">
+                            <div class="col-xs-10 col-sm-9 col-xs-offset-2 col-sm-offset-3 checkbox-content" id="category-checkboxes">
                                 <div class="checkboxCustom">
                                     <input disabled checked type="checkbox" name="allChb" id="allChb" data-category="all">
                                     <label for="allChb">All</label>
